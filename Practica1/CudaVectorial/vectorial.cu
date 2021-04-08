485eb8358048f4fd03b17125ba8bca56adfdb5f9
Dunspa <jlgallego99@correo.ugr.es>
2021-04-08 20:53:22 +0200

    Transformacion vectorial terminado

--- Practica1/CudaVectorial/vectorial.cu
+++ Practica1/CudaVectorial/vectorial.cu
@@ -7,66 +7,52 @@
 
 using namespace std;
 
-// Kernel de advección lineal (memoria compartida)
-__global__ void FD_kernel2(float * d_phi, float * d_phi_new, float cu, int n) { 
-    int li = threadIdx.x + 1;   // índice local del vector en memoria compartida
-	int gi = blockDim.x * blockIdx.x + threadIdx.x + 1; // índice global
-    int lstart = 0;   
-    int lend = BLOCKSIZE + 1;
-	float result;
-    __shared__ float s_phi[BLOCKSIZE + 2];
-
-   	// Almacenar en memoria compartida
-    if (gi < n + 2) {
-		s_phi[li] = d_phi[gi];
-	}  
-
-	// Primera hebra del bloque
-   	if (threadIdx.x == 0) {
-		s_phi[lstart] = d_phi[gi - 1]; 
-	}
-
-	// Última hebra
-   	if (threadIdx.x == BLOCKSIZE - 1)
-	   	// Último bloque  
-	  	if (gi >= n + 1)
-	      	s_phi[(n + 2) % BLOCKSIZE] = d_phi[n + 2];
-	  	else   
-	      	s_phi[lend] = d_phi[gi + 1];
-
-	// Esperar a que todas las hebras hayan llenado la memoria compartida
-  	__syncthreads();
-       
-   	if (gi < n + 2) {
-		// Lax-Friedrichs
-		result = 0.5 * ((s_phi[li + 1] + s_phi[li - 1])
-				- cu * (s_phi[li + 1] - s_phi[li - 1]));        
-		d_phi_new[gi] = result;
-    }
-    
-   	// Boundary Conditions
-    if (gi == 1) {
-		d_phi_new[0] = d_phi_new[1];
-	}
-
-    if (gi == n + 1) {
-		d_phi_new[n + 2] = d_phi_new[n + 1];
-	}
-}
-
 // Kernel que realiza el cálculo del vector B con memoria compartida
-__global__ vectorial_shared(float * A, float * B, const int n) {
+__global__ void vectorial_shared(float * A, float * B, const int n) {
+	extern __shared__ float sdata[];	// Datos en memoria compartida
+	int i = threadIdx.x + blockDim.x * blockIdx.x;	// Índice global de hebra
+	float Ai, Aim1, Aim2, Aip1, Aip2; 
 
+	if (i >= 2 && i < n + 2) {
+		// Almacenar A en memoria compartida
+		sdata[i] = A[i];
+
+		// Esperar a que todas las hebras terminen de llenar la memoria compartida
+		__syncthreads();
+
+		// Hacer la operación usando la memoria compartida
+		// Cada hebra se ocupa de una posición del vector B
+		Aim2 = sdata[i - 2];
+		Aim1 = sdata[i - 1];  
+		Ai = sdata[i];
+		Aip1 = sdata[i + 1];  
+		Aip2 = sdata[i + 2];
+		B[i] = (pow(Aim2, 2) + 2.0 * pow(Aim1, 2) + pow(Ai, 2) 
+		- 3.0 * pow(Aip1, 2) + 5.0 * pow(Aip2, 2)) / 24.0; 
+	}
 }
 
 // Kernel que realiza el cálculo del vector B en memoria global
-__global__ vectorial_global(float * A, float * B, const int n) {
+__global__ void vectorial_global(float * A, float * B, const int n) {
+	int i = threadIdx.x + blockDim.x * blockIdx.x;	// Índice global de hebra
+	float Ai, Aim1, Aim2, Aip1, Aip2; 
 
+	if (i >= 2 && i < n + 2) {
+		// Hacer la operación usando la memoria global
+		// Cada hebra se ocupa de una posición del vector B
+		Aim2 = B[i - 2];
+		Aim1 = B[i - 1];  
+		Ai = B[i];
+		Aip1 = B[i + 1];  
+		Aip2 = B[i + 2];
+		B[i] = (pow(Aim2, 2) + 2.0 * pow(Aim1, 2) + pow(Ai, 2) 
+		- 3.0 * pow(Aip1, 2) + 5.0 * pow(Aip2, 2)) / 24.0; 
+	}
 }
 
 // Kernel que calcula el valor máximo de entre los valores de un vector usando reduce
 __global__ void maximo_reduce(float * B, float * max, const int n) {
-	extern __shared__ int sdata[];	// Datos en memoria compartida
+	extern __shared__ float sdata[];	// Datos en memoria compartida
 
 	int tid = threadIdx.x;
 	int i = threadIdx.x + blockDim.x * blockIdx.x;	// Índice global de hebra
@@ -74,7 +60,7 @@ __global__ void maximo_reduce(float * B, float * max, const int n) {
 	// Almacenar vector en memoria compartida
 	// Comprobar que la hebra del bloque puede operar en el vector
 	// Si no, se le pone un valor demasiado bajo para que no pueda ser el máximo
-	sdata[tid] = (i < n + 2) ? A[i] : -1000000.0f;
+	sdata[tid] = (i < n + 2) ? B[i] : -1000000.0f;
 	// Esperar a que acaben todas las hebras para terminar de llenar la memoria compartida
 	__syncthreads();
 
@@ -100,12 +86,12 @@ __global__ void maximo_reduce(float * B, float * max, const int n) {
 }
 
 // Función para terminar de hacer el máximo con reduce en CPU
-return float maximo_reduce_cpu(float * h_max) {
+float maximo_reduce_cpu(float * h_max, int blocksPerGrid) {
 	// Terminar de computar el reduce en CPU
 	// Se calcula el máximo de los encontrados en los distintos bloques
 	float mx = -1000000.0f;
 	for (int i = 0 ; i < blocksPerGrid ; i++) {
-		mx = (longitudMaxima > h_max[i]) ? longitudMaxima : h_max[i];
+		mx = (mx > h_max[i]) ? mx : h_max[i];
 	}
 
 	return mx;
@@ -117,12 +103,16 @@ int main(int argc, char* argv[]) {
 		return(-1);
 	}
 	
+	// Gestionar errores cuda
+	cudaError_t err;
 	// Número de puntos
 	const int n = atoi(argv[1]);
 	// Tamaño de bloque
 	const int blocksize = atoi(argv[2]);
+	// Valor máximo
+	float mx = 0.0f;
 
-	// Inicializar vector A
+	// Inicializar vector A en CPU
 	float * A = new float[n + 4];
 	for (int i = 2 ; i < n + 2 ; i++) {
 		A[i] = ((float) rand()) / (float) RAND_MAX;
@@ -140,9 +130,12 @@ int main(int argc, char* argv[]) {
 		cout << "ERROR COPIA A GPU" << endl;
 	}
 
+	// Inicializar vector B en CPU
+	float * B = new float[n];
+
 	// Inicializar vector B en GPU
 	float * d_B = NULL;
-	err = cudaMalloc((void **) &, n*sizeof(float)); 
+	err = cudaMalloc((void **) &d_B, n*sizeof(float)); 
 	if (err != cudaSuccess) {
 		cout << "ERROR DE RESERVA DE MEMORIA" << endl;
 	}
@@ -150,7 +143,6 @@ int main(int argc, char* argv[]) {
 	// Obtener información de GPU
     int devID;
     cudaDeviceProp props;
-    cudaError_t err;
     err = cudaGetDevice(&devID);
     if (err != cudaSuccess) {
 		cout << "ERROR" << endl;
@@ -207,14 +199,14 @@ int main(int argc, char* argv[]) {
 	Tgpu2 = (Tgpu2 - t1) / CLOCKS_PER_SEC;
 
 	// Calcular el valor máximo de todos los valores almacenados en B
-	int * d_max; 
+	float * d_max; 
 	cudaMalloc ((void **) &d_max, sizeof(float)*blocksPerGrid);
 	maximo_reduce<<< blocksPerGrid, threadsPerBlock, smemSize >>>(B, d_max, n);	
 	float * h_max = (float*) malloc(blocksPerGrid*sizeof(float));
 	cudaMemcpy(h_max, d_max, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost);
 
 	// Terminar reduce en CPU
-	mx = maximo_reduce_cpu(h_max);
+	mx = maximo_reduce_cpu(h_max, blocksPerGrid);
 
 	// Mostrar tiempos de CPU, GPU SHARED, GPU GLOBAL, SPEEDUP SHARED, SPEEDUP GLOBAL
 	cout << Tcpu << " " << Tgpu1 << " " << Tgpu2 << " " 
