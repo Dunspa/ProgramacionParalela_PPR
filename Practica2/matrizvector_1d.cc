1c0bc0b368f4e8ff2672f2df659a83ebd2115ae3
Dunspa <jlgallego99@correo.ugr.es>
2021-05-01 19:26:28 +0200

    Algoritmo en 1d completamente funcional

--- Practica2/matrizvector_1d.cc
+++ Practica2/matrizvector_1d.cc
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    int rank, size;
-    int **A, *x, *y, *miFila, *comprueba;
+    int rank, size, n;
+    int **A, *x, *y, *miBloque, *comprueba, *subFinal;
     double tInicio, tFin;
     MPI_Status estado;
 
@@ -17,24 +17,55 @@ int main(int argc, char *argv[]) {
     // Obtenemos la identificación de nuestro proceso en el comunicador global
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Leer parámetros
+    if (argc < 2) {
+        cout << "Número de parámetros incorrecto, es necesario indicar el tamaño del vector" << endl;
+    } else {
+        n = atoi(argv[1]);
+        
+        // Comprobar si n es un múltiplo del número de procesos y es mucho mayor
+        // Como mínimo debe ser mayor que el doble del número de procesos
+        if (n % size == 0 && n >= 2 * size) {
+            if (rank == 0) {
+                cout << "Tamaño del vector: " << n << endl << endl;
+            }
+        } else {
+            if (rank == 0) {
+                cout << "El tamaño del vector indicado no es correcto." 
+                     << " Debe ser un múltiplo del número de procesos (" << size
+                     << ") y además debe ser mayor que el doble de este" << endl;
+            }
+            
+            // Transformar valor n a uno válido
+            do {
+                n++;
+            } while (n % size != 0);
+
+            if (rank == 0) {
+                cout << "Se ha transformado el valor para que sea correcto" << endl
+                     << "Tamaño del vector: " << n << endl << endl;
+            }
+        }
+    }
+
     // Reserva de vectores y matriz
-    A = new int *[size];
-    x = new int [size];
+    A = new int *[n];
+    x = new int [n];
 
     if (rank == 0) {
-        A[0] = new int [size * size];
-        for (unsigned int i = 1 ; i < size ; i++) {
-            A[i] = A[i - 1] + size;
+        A[0] = new int [n * n];
+        for (unsigned int i = 1 ; i < n ; i++) {
+            A[i] = A[i - 1] + n;
         }
 
         // Reservamos espacio para el resultado
-        y = new int [size];
+        y = new int [n];
 
         // Rellenamos A y x con valores aleatorios
         srand(time(0));
         cout << "La matriz y el vector generados son " << endl;
-        for (unsigned int i = 0 ; i < size ; i++) {
-            for (unsigned int j = 0 ; j < size ; j++) {
+        for (unsigned int i = 0 ; i < n ; i++) {
+            for (unsigned int j = 0 ; j < n ; j++) {
                 if (j == 0) {
                     cout << "[";
                 }
@@ -42,7 +73,7 @@ int main(int argc, char *argv[]) {
                 A[i][j] = rand() % 1000;
                 cout << A[i][j];
 
-                if (j == size - 1) {
+                if (j == n - 1) {
                     cout << "]";
                 } else {
                     cout << " ";
@@ -55,34 +86,47 @@ int main(int argc, char *argv[]) {
         cout << endl;
 
         // Reservamos espacio para la comprobación
-        comprueba = new int [size];
+        comprueba = new int [n];
 
         // Lo calculamos de forma secuencial
-        for (unsigned int i = 0 ; i < size ; i++) {
+        for (unsigned int i = 0 ; i < n ; i++) {
             comprueba[i] = 0;
             
-            for (unsigned int j = 0 ; j < size ; j++) {
+            for (unsigned int j = 0 ; j < n ; j++) {
                 comprueba[i] += A[i][j] * x[j];
             }
         }
     }
 
-    // Reservamos espacio para la fila local de cada proceso
-    miFila = new int [size];
+    // Número de elementos y filas de la matriz que tiene cada proceso
+    int nfilas = n / size;
+    int nelementos = (n * n) / size;
 
-    // Repartimos una fila por cada proceso
-    MPI_Scatter(A[0], size, MPI_INT, miFila, size, MPI_INT, 0, MPI_COMM_WORLD);
+    // Reservamos espacio para el bloque de filas local de cada proceso
+    miBloque = new int [nelementos];
+    // Reservamos espacio para los resultados que calcula cada proceso
+    // Uno por fila que tiene
+    subFinal = new int [nfilas];
+
+    // Repartimos n/p filas por cada proceso
+    MPI_Scatter(A[0], nelementos, MPI_INT, miBloque, nelementos, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Compartimos el vector entre todos los procesos
-    MPI_Bcast(x, size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(x, n, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Barrera para asegurar que todos los procesos comiencen a la vez
     MPI_Barrier(MPI_COMM_WORLD);
     tInicio = MPI_Wtime();
 
-    int subFinal = 0;
-    for (unsigned int i = 0 ; i < size ; i++) {
-        subFinal += miFila[i] * x[i];
+    // Se tiene más de un subvector final, se calcula cada vez un resultado
+    for (int i = 0 ; i < nfilas ; ++i) {
+        subFinal[i] = 0;
+
+        for (int j = 0 ; j < n ; j++) {
+            subFinal[i] += miBloque[j + (n * i)] * x[j];
+        }
+
+        cout << subFinal[i] << endl;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -90,7 +134,7 @@ int main(int argc, char *argv[]) {
 
     // Recogemos los escalares de la multiplicación en un vector
     // Se hace en el mismo orden que el scatter
-    MPI_Gather(&subFinal, 1, MPI_INT, y, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(&subFinal[0], 2, MPI_INT, y, 2, MPI_INT, 0, MPI_COMM_WORLD);
 
     MPI_Finalize();
 
@@ -98,7 +142,7 @@ int main(int argc, char *argv[]) {
         unsigned int errores = 0;
 
         cout << "El resultado obtenido y el esperado son: " << endl;
-        for (unsigned int i = 0 ; i < size ; i++) {
+        for (unsigned int i = 0 ; i < n ; i++) {
             cout << "\t" << y[i] << "\t|\t" << comprueba[i] << endl;
             if (comprueba[i] != y[i]) {
                 errores++;
@@ -119,5 +163,7 @@ int main(int argc, char *argv[]) {
 
     delete [] x;
     delete [] A;
-    delete [] miFila;
+    delete [] miBloque;
+
+    return 0;
 }
